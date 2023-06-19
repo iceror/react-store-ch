be3a4465026dcaebdcc22ae828407896d1317061
iceror <ilsecervor@gmail.com>
2023-06-18 22:25:20 -0600

    install react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import NavBar from "./Components/Navbar"
 import ItemListContainer from "./Components/ItemListContainer"
 import './App.css'
+import { BrowserRouter,Routes, Route } from "react-router-dom"
 
 
 function App() {
 
   return (
-    //IMPORTAR NAVBAR, DENTRO DE NAVBAR CARTWIDGET Y DEBAJO ITEMLIST
+    <BrowserRouter>
     <div>
       <NavBar name='La tiendita'>
       </NavBar>
-      <ItemListContainer></ItemListContainer>
+
+      <Routes>
+        {/* //Listado de componentes Route */}
+        <Route path="/" element={ <ItemListContainer/> }/>
+        <Route/>
+        <Route/>
+        <Route/>
+
+      </Routes>
+
+      {/* <Footer></Footer> */}
     </div>
+    </BrowserRouter>
   )
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,6 @@
+import { Link } from "react-router-dom";
 import CartWidget from "./Cart"
+
 
 
 const NavBar = (props) => {
