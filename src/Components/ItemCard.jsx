c2098a74cd60fcbfc22b19e537e0e020e60a2523
iceror <ilsecervor@gmail.com>
2023-06-19 21:39:45 -0600

    change route to category/itemid

--- src/Components/ItemCard.jsx
+++ src/Components/ItemCard.jsx
@@ -1,13 +1,17 @@
-const ItemCard = ({id, product_name, description, price, image}) => {
+import { Link } from "react-router-dom"
+
+const ItemCard = ({ id, product_name, description, price, image, category }) => {
 
   return (
+    <Link to={`/${category}/${id}`}>
       <div>
-          <h4>{product_name}</h4>
-          <img src={image} alt={product_name}/>
-          <p>{description}</p>
-          <p>USD ${price}</p>
-          <button>Ver más</button>
+        <h4>{product_name}</h4>
+        <img src={image} alt={product_name} />
+        <p>{description}</p>
+        <p>USD ${price}</p>
+        <button>Ver más</button>
       </div>
+    </Link>
   )
 }
 
