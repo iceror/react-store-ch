ceb17dee342c8f8eeda38cbe07b761226251036d
iceror <ilsecervor@gmail.com>
2023-06-20 20:06:50 -0600

    delete productos h2

--- src/Components/ItemList.jsx
+++ src/Components/ItemList.jsx
@@ -5,9 +5,6 @@ const ItemList = ({products}) => {
   console.log(products);
     return (
         <div>
-            <h2>Productos</h2>
-            <hr/>
-
             <div className='item-list'>
                 {
                     // items.map((prod) => <ItemCard key={prod.id} item={prod}/>)
