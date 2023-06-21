aad8dbdabb7bbb0d28dfc763b9b1237f9ba4fb07
iceror <ilsecervor@gmail.com>
2023-06-20 19:26:11 -0600

    delete not used functions

--- src/Components/ItemDetail.jsx
+++ src/Components/ItemDetail.jsx
@@ -2,31 +2,9 @@ import { Link, useNavigate } from "react-router-dom"
 import { useState } from "react"
 
 const ItemDetail = ({ id, product_name, description, price, image, category }) => {
-  // const [cantidad, setCantidad] = useState(null)
-  // const [talle, setTalle] = useState(null)
-
-  // const navigate = useNavigate()
-
-  // const handleAgregar = () => {
-  //     const item = {
-  //         id, 
-  //         product_name, 
-  //         price, 
-  //         category, 
-  //         description, 
-  //         image
-  //     }
-  //     console.log(item)
-  // }
-
-  // handleAgregar()
-  // const handleVolver = () => {
-  //     navigate(-1)
-  // }
-
 
   return (
-    <div className="container my-5">
+    <div>
       <h2>{product_name}</h2>
 
       <img src={image} alt={product_name} />
