ebf59be8a025d23fd32160f792070950e695fdfd
iceror <ilsecervor@gmail.com>
2023-07-09 23:51:03 -0600

    get products from firestore database

--- src/Components/ItemListContainer.jsx
+++ src/Components/ItemListContainer.jsx
@@ -3,25 +3,29 @@ import { getData } from '../helpers/getData'
 import ItemList from '../Components/ItemList'
 import { useParams } from 'react-router-dom'
 import { useSearchParams } from 'react-router-dom'
+import { collection, getDocs } from "firebase/firestore"
+import { database } from "../firebase/config"
 
 
 const ItemListContainer = () => {
   const [products, setProducts] = useState([])
   const [loading, setLoading] = useState(true)
   const [searchParams] = useSearchParams()
-  
+
   //const search = searchParams.get("search")
   const { categoryId } = useParams()
   useEffect(() => {
     setLoading(true)
-
-    getData()
-      .then((res) => {
+    // armar ref
+    const productsRef = collection(database, 'products');
+    getDocs(productsRef)
+      .then((response) => {
+        const productsArray = response.docs.map((doc) => doc.data())
+        console.log(productsArray)
         if (!categoryId) {
-          //console.log(res.products);
-          setProducts(res.products)
+          setProducts(productsArray)
         } else {
-          setProducts(res.products.filter((item) => item.category === categoryId))
+          setProducts(productsArray.filter((item) => item.category === categoryId))
         }
       })
       .catch((err) => console.log(err))
