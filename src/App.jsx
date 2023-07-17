9a8277cda8ae3e1e1464752f05b9748a22ea299a
iceror <ilsecervor@gmail.com>
2023-07-16 19:31:27 -0600

    wrap app in cartprovider

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import NavBar from "./Components/Navbar"
 import ItemListContainer from "./Components/ItemListContainer"
 import ItemDetailContainer from "./Components/ItemDetailContainer"
+import Cart from "./Components/Cart"
 import './App.css'
 import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+import { CartProvider } from "../Context/CartContext"
 
 
 function App() {
 
   return (
     <BrowserRouter>
+    <CartProvider>
     <div>
       <NavBar name='La tiendita'>
       </NavBar>
@@ -18,11 +21,13 @@ function App() {
         <Route path="/" element={<ItemListContainer/>} />
           <Route path="/:categoryId" element={<ItemListContainer/>} />
           <Route path="/:categoryId/:itemId" element={<ItemDetailContainer />}/>
+          <Route path="/carrito" element={<Cart/>}></Route>
           <Route path="*" element={ <Navigate to={"/"}/>}/>
         </Routes>
 
       {/* <Footer></Footer> */}
     </div>
+    </CartProvider>
     </BrowserRouter>
   )
 }
