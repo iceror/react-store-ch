5d1a0d09add0d23354a30ec6fbfb9b159d7d6387
iceror <ilsecervor@gmail.com>
2023-07-16 21:37:01 -0600

    format app.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,21 +4,22 @@ import CartWidget from "./CartWidget"
 
 
 const NavBar = (props) => {
-  return (<header>
-    <div className="header">
-      <Link to='/'><h1 className="header-brand" >{props.name}</h1></Link> 
-      <nav className="header-navbar">
-        <Link  className="header-link" to='/decoracion'>Decoración</Link>
-        <Link  className="header-link" to='/cocina'>Cocina</Link>
-        <Link  className="header-link" to='/recamara'>Recámara</Link>
-        <Link  className="header-link" to='/bano'>Baño</Link>
-        <Link  className="header-link" to='/oficina'>Oficina</Link>
-        <Link  className="header-link" to='/articulos-hogar'>Articulos Hogar</Link>
-      </nav>
-      <Link to='/carrito'><CartWidget/></Link>
-      
-    </div>
-  </header>
+  return (
+    <header>
+      <div className="header">
+        <Link to='/'><h1 className="header-brand" >{props.name}</h1></Link>
+        <nav className="header-navbar">
+          <Link className="header-link" to='/decoracion'>Decoración</Link>
+          <Link className="header-link" to='/cocina'>Cocina</Link>
+          <Link className="header-link" to='/recamara'>Recámara</Link>
+          <Link className="header-link" to='/bano'>Baño</Link>
+          <Link className="header-link" to='/oficina'>Oficina</Link>
+          <Link className="header-link" to='/articulos-hogar'>Articulos Hogar</Link>
+        </nav>
+        <Link to='/carrito'><CartWidget /></Link>
+
+      </div>
+    </header>
   )
 }
 
