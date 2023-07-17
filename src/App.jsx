21f096ebf7989cbe4c72a27e1ff008bfdf054c89
iceror <ilsecervor@gmail.com>
2023-07-16 21:35:50 -0600

    wrap app in CartProvider

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ItemListContainer from "./Components/ItemListContainer"
 import ItemDetailContainer from "./Components/ItemDetailContainer"
 import Cart from "./Components/Cart"
 import './App.css'
-import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import { CartProvider } from "../Context/CartContext"
 
 
@@ -11,23 +11,23 @@ function App() {
 
   return (
     <BrowserRouter>
-    <CartProvider>
-    <div>
-      <NavBar name='La tiendita'>
-      </NavBar>
+      <CartProvider>
+        <div>
+          <NavBar name='La tiendita'>
+          </NavBar>
 
-      <Routes>
-        {/* //Listado de componentes Route */}
-        <Route path="/" element={<ItemListContainer/>} />
-          <Route path="/:categoryId" element={<ItemListContainer/>} />
-          <Route path="/:categoryId/:itemId" element={<ItemDetailContainer />}/>
-          <Route path="/carrito" element={<Cart/>}></Route>
-          <Route path="*" element={ <Navigate to={"/"}/>}/>
-        </Routes>
+          <Routes>
+            {/* //Listado de componentes Route */}
+            <Route path="/" element={<ItemListContainer />} />
+            <Route path="/:categoryId" element={<ItemListContainer />} />
+            <Route path="/:categoryId/:itemId" element={<ItemDetailContainer />} />
+            <Route path="/carrito" element={<Cart />}></Route>
+            <Route path="*" element={<Navigate to={"/"} />} />
+          </Routes>
 
-      {/* <Footer></Footer> */}
-    </div>
-    </CartProvider>
+          {/* <Footer></Footer> */}
+        </div>
+      </CartProvider>
     </BrowserRouter>
   )
 }
