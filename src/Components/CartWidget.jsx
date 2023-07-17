ffcd42449d0b1ad60a23169be6e4a245c4df0d2e
iceror <ilsecervor@gmail.com>
2023-07-16 21:36:18 -0600

    add cart.length to span in notif dot

--- src/Components/CartWidget.jsx
+++ src/Components/CartWidget.jsx
@@ -1,14 +1,18 @@
 import cartImg from '../img/cart-img.png'
+import { useContext } from 'react'
+import { CartContext } from '../../Context/CartContext'
 
 const CartWidget = () => {
+  const { cart } = useContext(CartContext)
+  console.log('cart in CARTWIDGET ', cart);
+
   return (
     <section className="cart-widget">
       <div className="cart">
         <img src={cartImg} alt="" className="cart-img" />
-        <span id="notification-dot">1</span>
+        <span id="notification-dot">{cart.length}</span>
       </div>
     </section>
-
   )
 }
 
