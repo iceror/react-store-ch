ed073cfecea29db7053dbb4bf80f0c87e46f1633
iceror <ilsecervor@gmail.com>
2023-07-21 13:07:16 -0600

    update checkout, create order

--- src/Components/Checkout.jsx
+++ src/Components/Checkout.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { useCartContext } from "../../Context/CartContext"
-import CheckoutForm from "./CheckoutForm"
+import { Link, Navigate } from "react-router-dom"
 import { writeBatch, collection, where, documentId, addDoc, updateDoc, doc, getDoc, query, getDocs } from "firebase/firestore"
 import { database } from "../firebase/config"
 import { Formik, Form, Field, ErrorMessage } from "formik"
@@ -21,36 +21,42 @@ const schema = Yup.object().shape({
 })
 
 const Checkout = () => {
-  const { cart, purchaseTotal } = useCartContext()
+  const { cart, purchaseTotal, emptyCart } = useCartContext()
+
+
+  if (cart.length === 0) {
+    return <Navigate to="/"/>
+}
 
   const [orderId, setOrderId] = useState()
 
-  console.log(cart);
   const batch = writeBatch(database)
   const ordersRef = collection(database, 'orders')
   const productsRef = collection(database, 'products')
 
-  const q = query(productsRef, where(documentId(), "in", cart.map(item => item.id)))
-  // const products = await getDocs(q);
+  const getProductsInCart = () => {
+    const q = query(productsRef, where(documentId(), "in", cart.map(item => item.id)))
+    // const products = await getDocs(q);
 
-  const productsPromise = getDocs(q);
+    const productsPromise = getDocs(q);
 
-  productsPromise.then((products) => {
-    products.docs.forEach((doc) => {
-      const item = cart.find((prod) => prod.id === doc.id)
-      const stock = doc.data().quantity
+    productsPromise.then((products) => {
+      products.docs.forEach((doc) => {
+        const item = cart.find((prod) => prod.id === doc.id)
+        const stock = doc.data().quantity
 
-      if (stock >= item.cantidad) {
-        batch.update(doc.ref, {
-          stock: stock - item.count
-        })
-      } else {
-        outOfStock.push(item)
-      }
-    })
-  }).catch((error) => {
+        if (stock >= item.cantidad) {
+          batch.update(doc.ref, {
+            stock: stock - item.count
+          })
+        } else {
+          outOfStock.push(item)
+        }
+      })
+    }).catch((error) => {
+    });
+  }
 
-  });
 
   const createOrder = async (values) => {
     const order = {
@@ -59,67 +65,75 @@ const Checkout = () => {
       total: purchaseTotal(),
       date: new Date()
     }
+
+    const outOfStock = [];
+
+    if (outOfStock.length === 0) {
+      batch.commit()
+        .then(() => {
+          addDoc(ordersRef, order)
+            .then((doc) => {
+              setOrderId(doc.id)
+            })
+            .catch(err => console.log(err))
+            .finally(() => {
+              emptyCart()
+            })
+        })
+    } else {
+      alert("Hay items sin stock")
+    }
+    return order
   }
 
-  // const outOfStock = [];
-
-  // if (outOfStock.length === 0) {
-  //   batch.commit()
-  //     .then(() => {
-  //       addDoc(ordersRef, order)
-  //         .then((doc) => {
-  //           setOrderId(doc.id)
-  //           vaciarCarrito()
-  //         })
-  //         .catch(err => console.log(err))
-  //     })
-  // } else {
-  //   alert("Hay items sin stock")
-  // }
-  if (cart.length === 0) {
-    return <Navigate to="/" />
+  if (orderId && cart.length === 0) {
+    return (
+      <div className="successful">
+        <h2 className="success">¡Tu compra ha sido registrada!</h2>
+        <hr />
+        <p className="order-num">Tu id de orden es: {orderId}</p>
+        <Link to="/"><button className="back-to-store">Regresar a la tienda</button></Link>
+      </div>
+    )
+  } else {
+    getProductsInCart()
+    return (
+      <div>
+        <h2>Checkout</h2>
+        <hr />
+        <Formik
+          initialValues={{
+            nombre: '',
+            direccion: '',
+            email: ''
+          }}
+          onSubmit={createOrder}
+          validationSchema={schema}
+        >
+          {() => (
+            <Form className="checkout-form">
+              <div>
+                <label htmlFor="nombre">Nombre</label>
+                <Field type="text" name="nombre" className="checkout-form-field" placeholder='Nombre' />
+                <ErrorMessage name="nombre" component={"p"} className="error-message" />
+              </div>
+              <div>
+                <label htmlFor="direccion">Dirección</label>
+                <Field type="text" name="direccion" className="checkout-form-field" placeholder='Calle y número' />
+                <ErrorMessage name="direccion" component={"p"} className="error-message" />
+              </div>
+              <div>
+                <label htmlFor="email">Email</label>
+                <Field type="email" name="email" className="checkout-form-field" placeholder='Email' />
+                <ErrorMessage name="email" component={"p"} className="error-message" />
+              </div>
+              <button type="submit">Enviar</button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    )
   }
-
-  return (
-    <div>
-      <h2>Checkout</h2>
-      <hr />
-      {/* <CheckoutForm /> */}
-      <Formik
-        initialValues={{
-          nombre: '',
-          direccion: '',
-          email: ''
-        }}
-        onSubmit={createOrder}
-        validationSchema={schema}
-      >
-        {() => (
-          <Form className="checkout-form">
-            <div>
-              <label htmlFor="name">Nombre</label>
-              <Field type="text" name="name" className="checkout-form-field" placeholder='Nombre' />
-              <ErrorMessage name="nombre" component={"p"} className="error-message"/>
-            </div>
-            <div>
-              <label htmlFor="address">Dirección</label>
-              <Field type="text" name="address" className="checkout-form-field" placeholder='Calle y número' />
-              <ErrorMessage name="direccion" component={"p"} className="error-message" />
-            </div>
-            <div>
-              <label htmlFor="email">Email</label>
-              <Field type="email" name="email" className="checkout-form-field" placeholder='Email' />
-              <ErrorMessage name="email" component={"p"} className="error-message" />
-            </div>
-            <button >Enviar</button>
-          </Form>
-        )}
-      </Formik>
-
-    </div>
-
-  )
-
 }
 
 export default Checkout
